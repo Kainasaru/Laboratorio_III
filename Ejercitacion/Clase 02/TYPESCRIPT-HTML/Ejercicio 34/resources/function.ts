7f7893fead3989f853b64baff033257337afe5e8
Kainasaru <kainasaru@gmail.com>
2019-09-06 12:09:54 -0300

    Ejercitacion clase 03

--- Ejercitacion/Clase 02/TYPESCRIPT-HTML/Ejercicio 34/resources/function.ts
+++ Ejercitacion/Clase 02/TYPESCRIPT-HTML/Ejercicio 34/resources/function.ts
@@ -1,17 +1,17 @@
 /*34- Agregarle al ejercicio del ingreso de datos la siguiente funcionalidad:
 a. Colocar todos los controles deshabilitados (disabled) menos el del número de trámite, que además deberá tener foco.
-b. Al ingresar un valor y pulsar ‘enter’ se invocará a una función que se encargue de validar dicho control y 
+b. Al ingresar un valor y pulsar ‘enter’ se invocará a una función que se encargue de validar dicho control y
 de habilitar y pasarle el foco al control siguiente, únicamente si el valor del control es válido.
 c. Si el usuario pulsa la tecla ‘esc’ el control deberá deshabilitarse y el foco será pasado al control anterior.*/
 function focusAndEnable(control: HTMLInputElement) {
     let lastChar: string = (<KeyboardEvent>event).key;
     let inputValue: string = control.value;
-    console.log( control.value);
-    if (lastChar == "Enter" && validar(inputValue)) { //Verifico que los valoreas sean 0
+    console.log( lastChar);
+    if (lastChar === "Enter" && validar(inputValue)) {
         (<HTMLInputElement>document.getElementById((parseInt(control.id) + 1).toString())).disabled = false;
         setFocus( (parseInt(control.id) + 1).toString());
     }
-    else if (lastChar == "Escape" && validar(inputValue) && parseInt(control.id) > 1) {
+    else if (lastChar === "Escape" && validar(inputValue) && parseInt(control.id) > 1) {
         console.log("entre");
         (<HTMLInputElement>control).disabled = true;
         control.value = "";
