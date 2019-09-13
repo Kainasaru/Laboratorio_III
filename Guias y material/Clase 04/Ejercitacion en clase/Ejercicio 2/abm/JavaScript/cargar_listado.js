6b1fb7bc9945c5ee00a85b040e3ff29d9ed15220
Kainasaru <kainasaru@gmail.com>
2019-09-13 15:15:32 -0300

    Finalizado ejercicio en clase - parte de la clase

--- Guias y material/Clase 04/Ejercitacion en clase/Ejercicio 2/abm/JavaScript/cargar_listado.js
+++ Guias y material/Clase 04/Ejercitacion en clase/Ejercicio 2/abm/JavaScript/cargar_listado.js
@@ -1,10 +1,8 @@
 window.onload = function () {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        console.log("estoy");
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            //window.location.href = "./login/login.php";
-            console.log("Respuesta: " + xhttp.response);
+        if (xhttp.readyState == 4 && xhttp.status == 200 && xhttp.response == "0") {
+            window.location.href = "./login/login.php";
         }
     };
     xhttp.open("GET", "./verificacion.php?verify=user", true);
