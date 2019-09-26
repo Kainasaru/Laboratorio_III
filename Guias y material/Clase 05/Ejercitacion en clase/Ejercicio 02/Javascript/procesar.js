c287dd3f9ccdb27d679ef99a8d04bd33032f17da
Kainasaru <kainasaru@gmail.com>
2019-09-26 10:34:27 -0300

    Finalizada ejercitacion en clase

--- Guias y material/Clase 05/Ejercitacion en clase/Ejercicio 02/Javascript/procesar.js
+++ Guias y material/Clase 05/Ejercitacion en clase/Ejercicio 02/Javascript/procesar.js
@@ -1,19 +1,16 @@
-function mostrar(obj) {
-    var jsonObj = JSON.parse(obj);
-    alert(jsonObj.Id + " - " + jsonObj.Marca + " - " + jsonObj.Precio + " - " + jsonObj.Color + " - " + jsonObj.Modelo);
+function mostrar(jsonStr) {
+    var jsonObj = JSON.parse(jsonStr);
+    alert("Id: " + jsonObj.Id + " - Marca: " + jsonObj.Marca + " -  Precio: " + jsonObj.Precio + " -  Color: " + jsonObj.Color + " -     Modelo: " + jsonObj.Modelo + ".");
 }
 function leer() {
+    var tabla = "<table border='1'><thead><th>Id</th><th>Marca</th><th>Precio</th><th>Color</th><th>Modelo</th>\
+    <th>Action</th></thead><tbody>";
     var xhttp = new XMLHttpRequest();
-    var tabla = "<table border=\"1\"><thead><th>Id</th><th>Marca</th><th>Precio</th><th>Color</th>" +
-        "<th>Modelo</th><th>Acciones</th></thead><tbody>";
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-            var json = JSON.parse(xhttp.response);
-            console.log(json);
-            for (var i = 0; i < json.length; i++) {
-                tabla += "<tr>";
-                tabla += "<td>" + json[i].Id + "</td><td>" + json[i].Marca + "</td><td>" + json[i].Precio + "</td><td>" + json[i].Color + "</td><td>" + json[i].Modelo + "</td><td>                <input type=\"button\" value=\"Ver\" onclick=\"mostrar('" + JSON.stringify(json[i]) + "')\" /></td>";
-                tabla += "</tr>";
+            var jsonObj = JSON.parse(xhttp.responseText);
+            for (var i = 0; i < jsonObj.length; i++) {
+                tabla += "<tr>                <td>" + jsonObj[i].Id + "</td>                <td>" + jsonObj[i].Marca + "</td>                <td>" + jsonObj[i].Precio + "</td>                <td>" + jsonObj[i].Color + "</td>                <td>" + jsonObj[i].Modelo + "</td>                <td><input type='button' value='ver' style='width:5em;'                 onclick='mostrar(" + JSON.stringify(JSON.stringify(jsonObj[i])) + ")' ></td>                </tr>";
             }
             tabla += "</tbody></table>";
             document.getElementById("result").innerHTML += tabla;
--- Guias y material/Clase 05/Ejercitacion en clase/Ejercicio 02/Javascript/procesar.ts
+++ Guias y material/Clase 05/Ejercitacion en clase/Ejercicio 02/Javascript/procesar.ts
@@ -1,27 +1,30 @@
-function mostrar(obj : string) {
-    let jsonObj = JSON.parse(obj);
-    alert(`${jsonObj.Id} - ${jsonObj.Marca} - ${jsonObj.Precio} - ${jsonObj.Color} - ${jsonObj.Modelo}`);
+function mostrar(jsonStr : string) {
+    let jsonObj = JSON.parse(jsonStr );
+    alert(`Id: ${jsonObj.Id} - Marca: ${jsonObj.Marca} -  Precio: ${jsonObj.Precio} -  Color: ${jsonObj.Color} - \
+    Modelo: ${jsonObj.Modelo}.`);
 }
 function leer() {
+    let tabla : string = "<table border='1'><thead><th>Id</th><th>Marca</th><th>Precio</th><th>Color</th><th>Modelo</th>\
+    <th>Action</th></thead><tbody>";
     let xhttp = new XMLHttpRequest();
-    let tabla = "<table border=\"1\"><thead><th>Id</th><th>Marca</th><th>Precio</th><th>Color</th>" +
-        "<th>Modelo</th><th>Acciones</th></thead><tbody>";
     xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            let json = JSON.parse(xhttp.response);
-            console.log(json);
-            for (let i = 0; i < json.length; i++) {
-                tabla += "<tr>";
-                tabla += `<td>${json[i].Id}</td><td>${json[i].Marca}</td><td>${json[i].Precio}</td><td>${json[i].Color}</td><td>${json[i].Modelo}</td><td>\
-                <input type="button" value="Ver" onclick="mostrar('${JSON.stringify(json[i])}')" /></td>`;
-                tabla += "</tr>";
+        if( xhttp.readyState == 4 && xhttp.status == 200) {
+            let jsonObj = JSON.parse(xhttp.responseText);
+            for(let i = 0 ; i < jsonObj.length ; i++) {
+                tabla += `<tr>\
+                <td>${jsonObj[i].Id}</td>\
+                <td>${jsonObj[i].Marca}</td>\
+                <td>${jsonObj[i].Precio}</td>\
+                <td>${jsonObj[i].Color}</td>\
+                <td>${jsonObj[i].Modelo}</td>\
+                <td><input type='button' value='ver' style='width:5em;'\
+                 onclick='mostrar(${JSON.stringify(JSON.stringify(jsonObj[i]))})' ></td>\
+                </tr>`;
             }
             tabla += "</tbody></table>";
-            document.getElementById("result").innerHTML += tabla;
+            (<HTMLDivElement>document.getElementById("result")).innerHTML += tabla;
         }
     }
     xhttp.open("GET", "./json_test.php", true);
     xhttp.send();
 }
-
-
