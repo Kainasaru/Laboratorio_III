61582b7c622e3d9435bf43706783f42f6c60ce34
Kainasaru <kainasaru@gmail.com>
2019-10-01 19:07:06 -0300

    Finalizada clase 02

--- Ejercitacion/Clase 02/TYPESCRIPT-HTML/Ejercicio 34/resources/function.js
+++ Ejercitacion/Clase 02/TYPESCRIPT-HTML/Ejercicio 34/resources/function.js
@@ -3,22 +3,37 @@ a. Colocar todos los controles deshabilitados (disabled) menos el del número de
 b. Al ingresar un valor y pulsar ‘enter’ se invocará a una función que se encargue de validar dicho control y
 de habilitar y pasarle el foco al control siguiente, únicamente si el valor del control es válido.
 c. Si el usuario pulsa la tecla ‘esc’ el control deberá deshabilitarse y el foco será pasado al control anterior.*/
-function focusAndEnable(control) {
+function focusNextPrev(control) {
     var lastChar = event.keyCode;
-    var nextControl = document.getElementById((Number(control.id) + 1).toString());
-    console.log(lastChar);
-    if (lastChar === 13 /*&& validar(inputValue)*/) {
-        control.disabled = true;
-        setFocus(nextControl);
-        nextControl.disabled = false;
+    var nextControl = null;
+    var prevControl = null;
+    if (control.nodeName == "TEXTAREA") {
+        control = control;
+        nextControl = document.getElementById((Number(control.id) + 1).toString());
+        prevControl = document.getElementById((Number(control.id) - 1).toString());
     }
-    else if (lastChar === 27 /*&& validar(inputValue) && parseInt(control.id) > 1*/) {
-        console.log("entre");
+    else if (control.nodeName == "SELECT") {
+        control = control;
+        nextControl = document.getElementById((Number(control.id) + 1).toString());
+        prevControl = document.getElementById((Number(control.id) - 1).toString());
+    }
+    else if (control.nodeName == "INPUT") {
+        control = control;
+        nextControl = document.getElementById((Number(control.id) + 1).toString());
+        prevControl = document.getElementById((Number(control.id) - 1).toString());
+    }
+    if (lastChar === 13 && validar(control.value)) {
+        nextControl.disabled = false;
+        setFocus(nextControl);
+    }
+    else if (lastChar === 27 && Number(control.id) > 1) {
+        control.disabled = true;
+        setFocus(prevControl);
     }
 }
 function setFocus(input) {
     input.focus();
 }
 function validar(str) {
-    return !isNaN(Number(str));
+    return str != "";
 }
--- Ejercitacion/Clase 02/TYPESCRIPT-HTML/Ejercicio 34/resources/function.ts
+++ Ejercitacion/Clase 02/TYPESCRIPT-HTML/Ejercicio 34/resources/function.ts
@@ -3,22 +3,39 @@ a. Colocar todos los controles deshabilitados (disabled) menos el del número de
 b. Al ingresar un valor y pulsar ‘enter’ se invocará a una función que se encargue de validar dicho control y
 de habilitar y pasarle el foco al control siguiente, únicamente si el valor del control es válido.
 c. Si el usuario pulsa la tecla ‘esc’ el control deberá deshabilitarse y el foco será pasado al control anterior.*/
-function focusAndEnable(control: HTMLInputElement) {
+
+function focusNextPrev(control : HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement) {
     let lastChar: number = (<KeyboardEvent>event).keyCode;
-    let nextControl : HTMLInputElement = <HTMLInputElement>document.getElementById( (Number(control.id)+1).toString() );
-    console.log( lastChar);
-    if (lastChar === 13 /*&& validar(inputValue)*/) {
-        control.disabled = true;
-        setFocus( nextControl);
+    let nextControl : HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement = null;
+    let prevControl : HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement  = null;
+    if(control.nodeName == "TEXTAREA") {
+        control = <HTMLTextAreaElement>control;
+        nextControl = <HTMLTextAreaElement>document.getElementById( (Number(control.id)+1).toString() );
+        prevControl = <HTMLTextAreaElement>document.getElementById((Number(control.id)-1).toString() );
+    }
+    else if( control.nodeName == "SELECT") {
+        control = <HTMLSelectElement>control;
+        nextControl = <HTMLSelectElement>document.getElementById( (Number(control.id)+1).toString() );
+        prevControl = <HTMLSelectElement>document.getElementById((Number(control.id)-1).toString() );
+    }
+    else if( control.nodeName == "INPUT") {
+        control = <HTMLInputElement>control;
+        nextControl = <HTMLInputElement>document.getElementById( (Number(control.id)+1).toString() );
+        prevControl = <HTMLInputElement>document.getElementById((Number(control.id)-1).toString() );
+    }
+    if (lastChar === 13 && validar(control.value)) {
         nextControl.disabled = false;
+        setFocus( nextControl );
       }
-    else if (lastChar === 27 /*&& validar(inputValue) && parseInt(control.id) > 1*/) {
-        console.log("entre");
+    else if (lastChar === 27 && Number(control.id) > 1) {
+        control.disabled = true;
+        setFocus(prevControl);
     }
 }
 function setFocus( input : HTMLElement  ) {
     input.focus();
 }
+
 function validar(str: string): boolean {
-    return !isNaN(Number(str));
+    return str != "";
 }
